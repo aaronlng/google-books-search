c4c5f72ee659e18f9208a78aec65ac8f1fc7bfef
Aaron Long <aaronlng05@gmail.com>
2019-07-07 08:46:50 -0400

    npm run build

---
+++ client/build/precache-manifest.e6a1c3d2335ffc9aa38836e5efdb66cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6efb7e830d30af262948",
+    "url": "/static/js/main.1916d65a.chunk.js"
+  },
+  {
+    "revision": "0b72d9e3460ea8854bdf",
+    "url": "/static/js/2.21703f8b.chunk.js"
+  },
+  {
+    "revision": "6efb7e830d30af262948",
+    "url": "/static/css/main.672ffbf8.chunk.css"
+  },
+  {
+    "revision": "26ed4012d6bd330d4dda839ceafab869",
+    "url": "/index.html"
+  }
+];
